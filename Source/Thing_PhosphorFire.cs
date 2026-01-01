223ca105fa7ab3688afa8d151211459abdc54ed2
TwoLargeGayMen <45555017+TwoLargeGayMen@users.noreply.github.com>
2026-01-01 17:22:53 -0600

    fire implented, needs to be animated

--- Source/Thing_PhosphorFire.cs
+++ Source/Thing_PhosphorFire.cs
@@ -1,39 +1,90 @@
-using Verse;
 using RimWorld;
+using Verse;
 using UnityEngine;
 
-namespace SegCOTO
+namespace Seg.COTO
 {
-    public class Thing_PhosphorFire : Fire
+    public class PhosphorFire : ThingWithComps
     {
-        static Thing_PhosphorFire()
+        private int ageTicks;
+        private const int LifetimeTicks = 10000;
+        private float fireSize = 1.0f;
+
+        protected override void Tick()
         {
-            Log.Message("SegCOTO: Thing_PhosphorFire class loaded successfully. Thank the Omnissiah!");
+            base.Tick();
+
+            ageTicks++;
+            if (ageTicks >= LifetimeTicks)
+            {
+                Destroy(DestroyMode.Vanish);
+                return;
+            }
+
+            if (!Spawned || Map == null)
+                return;
+
+            if (this.IsHashIntervalTick(60))
+                DoDamageAndHeat();
+
+            if (this.IsHashIntervalTick(120))
+                TrySpread();
+        }
+
+        private void DoDamageAndHeat()
+        {
+            var list = Map.thingGrid.ThingsListAt(Position);
+            for (int i = 0; i < list.Count; i++)
+            {
+                Thing t = list[i];
+                if (t == this)
+                    continue;
+
+                if (t.def.category == ThingCategory.Mote)
+                    continue;
+
+                float dmg = 5f;
+                t.TakeDamage(new DamageInfo(DamageDefOf.Flame, dmg, instigator: this));
+            }
+
+            float energy = fireSize * 160f;
+            GenTemperature.PushHeat(Position, Map, energy);
+
+            if (Rand.Value < 0.4f)
+                WeatherBuildupUtility.AddSnowRadial(Position, Map, fireSize * 3f, -(fireSize * 0.1f));
+        }
+
+        private void TrySpread()
+        {
+            IntVec3 c = Position + GenRadial.ManualRadialPattern[Rand.RangeInclusive(1, 8)];
+            if (!c.InBounds(Map))
+                return;
+
+            if (!c.Walkable(Map))
+                return;
+
+            if (Map.thingGrid.ThingAt<PhosphorFire>(c) != null)
+                return;
+
+            var fire = (PhosphorFire)ThingMaker.MakeThing(ThingDef.Named("Seg_COTO_PhosphorFire"));
+            GenSpawn.Spawn(fire, c, Map);
         }
     }
 
-    public class CompProperties_PhosphorFire : CompProperties
+    public class Projectile_Phosphor : Bullet
     {
-        public CompProperties_PhosphorFire()
+        protected override void Impact(Thing hitThing, bool blockedByShield = false)
         {
-            this.compClass = typeof(CompPhosphorFire);
-        }
-    }
+            Map map = Map;
+            IntVec3 pos = Position;
 
-    public class CompPhosphorFire : ThingComp
-    {
-        private int phosphorTickCounter;
+            base.Impact(hitThing, blockedByShield);
 
-        public CompProperties_PhosphorFire Props
-        {
-            get { return (CompProperties_PhosphorFire)this.props; }
-        }
+            if (map == null)
+                return;
 
-        public override void CompTick()
-        {
-            base.CompTick();
-            phosphorTickCounter++;
+            var fire = (PhosphorFire)ThingMaker.MakeThing(ThingDef.Named("Seg_COTO_PhosphorFire"));
+            GenSpawn.Spawn(fire, pos, map);
         }
     }
 }
-
