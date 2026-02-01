e07e7915e5131eae7a3b84bfc340a3d7979f9d0a
TwoLargeGayMen <45555017+TwoLargeGayMen@users.noreply.github.com>
2026-01-31 23:59:59 -0600

    RANKS NEEDS1/31 FRAMEWORK FROM PHO TO WORK

---
+++ Source/RanksParts.cs
@@ -0,0 +1,53 @@
+using RimWorld;
+using Verse;
+using System.Text;
+using System.Linq;
+using Core40k;
+
+namespace Seg
+{
+    public class RankDef : Core40k.RankDef
+    {
+        public int ImplantsRequired = 0;
+
+        public override bool  RequirementMet(
+            StringBuilder sb,
+            Pawn pawn,
+            CompRankInfo rankComp,
+            RankCategoryDef currentCategory,
+            out string reason)
+        {
+            if (ImplantsRequired > 0)
+            {
+                int installed = pawn.health.hediffSet.hediffs
+                    .OfType<Hediff_AddedPart>()
+                    .Count();
+
+                if (installed < ImplantsRequired)
+                {
+                    sb.AppendLine($"Requires at least {ImplantsRequired} devotions to the Omnissiah, known also as artificial implants (has {installed}).");
+                }
+            }
+
+            return base.RequirementMet(sb, pawn, rankComp, currentCategory, out reason);
+        }
+
+        public override string BuildRankBonusString(StringBuilder sb)
+        {
+            if (ImplantsRequired > 0)
+            {
+                sb.AppendLine($"• Requires {ImplantsRequired} devotions to the Omnissiah to unlock.");
+            }
+
+            return base.BuildRankBonusString(sb);
+        }
+
+        public override void UnlockRank(Pawn pawn, CompRankInfo rankComp)
+        {
+        }
+
+        public override void RemoveRank(Pawn pawn, CompRankInfo rankComp)
+        {
+        }
+    }
+}
